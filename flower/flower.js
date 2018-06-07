b61f4d1104c14d9788fd4df4e7dd285d78196a7f
lauranovruzaj <laura.novruzaj@gmail.com>
2018-06-07 13:17:01 +0200

    fixess

--- flower/flower.js
+++ flower/flower.js
@@ -5,4 +5,13 @@ $( document ).ready(function() {
     $(".section-3 a").mouseleave(function () {
         $(this).find(".price"). addClass("hidden");
     })
+
+   /* slick slider event*/
+    $(".vertical-center-4").slick({
+        dots: false,
+        autoplay:true,
+        arrows: false,
+        autoplaySpeed: 2000
+
+    });
 });
